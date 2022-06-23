e7c1fd7e97bb7abafab2c91622af2674723b7eaf
vicky7407 <vinayrathod5475@gmil.com>
2022-06-23 15:52:17 +0530

    Rect ui menue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import DrawerLayout from"./Components/DrawerLayout"
+import { Switch,Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DrawerLayout>
+        <Switch>
+          <Route>
+            
+          </Route>
+        </Switch>
+      </DrawerLayout>
     </div>
   );
-}
+};
 
 export default App;
